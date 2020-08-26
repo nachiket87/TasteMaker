54d8f25126d2f6a6e24933c8970ce85c5de1f9e7
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-26 17:54:09 -0400

    Added notifications :rocket:

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,9 +27,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { showNotificationContent } from '../components/navbar';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  
+  showNotificationContent();
 });
