24c2ae113f296b9d37ecc01262bd35d308c0df36
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-27 12:42:16 -0400

    Changed notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,6 @@ class NotificationsController < ApplicationController
   def destroy
     @notification = Notification.find(params[:id])
     @notification.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
