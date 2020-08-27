4043882d032cdef6393380fdb92261bcf041bdc3
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-27 13:07:02 -0400

    Removed a useless action in notifications_controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,11 +4,4 @@ class NotificationsController < ApplicationController
     @notification.destroy
     redirect_back(fallback_location: root_path)
   end
-
-  def join_game
-    @notification = Notification.find(params[:id])
-    @my_game = @notification.game
-    @notification.destroy
-    redirect_to :game_path
-  end
 end
