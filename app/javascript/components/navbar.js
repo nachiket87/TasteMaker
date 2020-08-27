5e72c8d00f0ff03723e940ce7f7505775d1ccd2a
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-27 17:15:22 -0400

    Added destroy logic to accept button in notifications

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -8,12 +8,20 @@ const showNotificationContent = () => {
     });
   }
   const declines = document.querySelectorAll(".decline-button");
-    declines.forEach((decline) => {
-      decline.addEventListener('click', (event) => {
-        event.preventDefault();
-        decline.parentElement.innerHTML = "";
-      });
+  declines.forEach((decline) => {
+    decline.addEventListener('click', (event) => {
+      event.preventDefault();
+      decline.parentElement.innerHTML = "";
     });
+  });
+
+  const accepts = document.querySelectorAll(".accept-button");
+  accept.forEach((accept) => {
+    accept.addEventListener('click', (event) => {
+      event.preventDefault();
+      accept.parentElement.innerHTML = "";
+    });
+  });
 }
 
 
