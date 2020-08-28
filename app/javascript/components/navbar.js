df73eed16ce8172abc22d7f0e5b6982aad77b0f4
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-08-28 14:48:07 -0400

    Added number to notification bell

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,15 @@
 const showNotificationContent = () => {
   const button = document.getElementById("dLabel");
-  const notiList = document.getElementById("notificationContainer")
+  const notiList = document.getElementById("notificationContainer");
+  const notifications = document.querySelectorAll(".notification-div");
   // if (button) {
   //   button.addEventListener('click', (event) => {
   //     event.preventDefault();
   //     notiList.classList.toggle("show");
   //   });
   // }
+  // console.log(accepts.length);
+
   const declines = document.querySelectorAll(".decline-button");
   declines.forEach((decline) => {
     decline.addEventListener('click', (event) => {
@@ -14,14 +17,25 @@ const showNotificationContent = () => {
       decline.parentElement.innerHTML = "";
     });
   });
+  let totalNotifications = document.getElementById("notification-number");
+  let numberNotifications = notifications.length
 
-  // const accepts = document.querySelectorAll(".accept-button");
-  // accepts.forEach((accept) => {
-  //   accept.addEventListener('click', (event) => {
-  //     event.preventDefault();
-  //     accept.parentElement.innerHTML = "";
-  //   });
-  // });
+  const accepts = document.querySelectorAll(".accept-button");
+  accepts.forEach((accept) => {
+    accept.addEventListener('click', (event) => {
+      accept.parentElement.innerHTML = "";
+    });
+  });
+
+  const notiButtons = document.querySelectorAll(".noti-btn");
+  notiButtons.forEach((notiButton) => {
+    notiButton.addEventListener('click', (event) => {
+      numberNotifications -= 1;
+      totalNotifications.innerHTML = numberNotifications;
+    });
+  });
+
+  totalNotifications.innerHTML = numberNotifications;
 }
 
 
