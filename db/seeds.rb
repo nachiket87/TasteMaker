12e8558710e929e683355ac3df3ce58baa906212
Nachiket Pusalkar <nachiket87@gmail.com>
2020-09-02 14:41:24 -0400

    changed a few typos in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ question3 = Question.create!(:link => "66615298", :answers => { correct: "Titani
 question4 = Question.create!(:link => "289488598", :answers => { correct: "Lord of the Rings", incorrect: ["The Fast & The Furious", "Cars", "Iron Eagle"] })
 question5 = Question.create!(:link => "119857523", :answers => { correct: "Mulan", incorrect: ["Frozen", "Tangled", "Pocahontas"] })
 question6 = Question.create!(:link => "35124119", :answers => { correct: "Planet of the Apes", incorrect: ["Santa Claus", "The Incredibles", "The Lion King"] })
-question7 = Question.create!(:link => "22612157", :answers => { correct: "Trainspotting", incorrect: ["Frozen", "The Incredibles", "Moana"] })
+question7 = Question.create!(:link => "22612157", :answers => { correct: "Trainspotting", incorrect: ["The Departed", "Crash", "Snatch"] })
 question8 = Question.create!(:link => "101315181", :answers => { correct: "The Imitation Game", incorrect: ["Hitler: Rise of Evil", "The Incredibles", "Jaws"] })
 question9 = Question.create!(:link => "3088877", :answers => { correct: "Office Space", incorrect: ["Moana", "The Incredibles", "Pinocchio"] })
 question10 = Question.create!(:link => "36364990", :answers => { correct: "Braveheart", incorrect: ["The Lost Kingdom", "Norsemen", "Kingdom of Heaven"] })
@@ -17,8 +17,8 @@ question11 = Question.create!(:link => "26022168", :answers => { correct: "Pulp 
 question12 = Question.create!(:link => "63394616", :answers => { correct: "Inglorious Basterds", incorrect: ["Hitler: Rise of Evil", "Saving Private Ryan", "Pearl Harbor"] })
 question13 = Question.create!(:link => "33257142", :answers => { correct: "The Departed", incorrect: ["The Aviator", "Scarface", "Gangs of New York"] })
 question14 = Question.create!(:link => "211088460", :answers => { correct: "Gladiator", incorrect: ["Centurion", "Ben-Hur", "Spartacus"] })
-question15 = Question.create!(:link => "4944032", :answers => { correct: "Casino Royal", incorrect: ["Golden Eye", "Quantum of Solace", "Goldfinger"] })
+question15 = Question.create!(:link => "4944032", :answers => { correct: "Casino Royale", incorrect: ["Golden Eye", "Quantum of Solace", "Goldfinger"] })
 question16 = Question.create!(:link => "134807219", :answers => { correct: "Anchorman 2", incorrect: ["Goodfellas", "Casino", "The Godfather"] })
 
-puts Question.all.answers.correct
-puts "Completed"
+puts Question.all
+puts "completed"
